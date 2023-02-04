7e6f40346e9e94c0eadc39f24ac1249806cb31f9
Felix <gunnarsson901@gmail.com>
2023-02-04 20:18:27 +0000

    add values to num1 and num2

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,14 +7,31 @@ document.addEventListener("DOMContentLoaded", function(){
                 alert('You clicked submit!');
             }else{
                 let gameType = this.getAttribute('data-type');
-                alert(`you clicked ${gameType}`)
+                runGame(gameType);
             }
-        });
-    }  
+        })
+    }
+    
+    runGame('addition');
+
 })
 
-function runGame(){
+/**
+ * The main game "loop", called when the script is loaded and 
+ * the users answer has been processed
+ */
+function runGame(gameType){
 
+    // create two random numbers between 1 and 25
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if(gameType === 'addition') {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`unknown game type: ${gameType}`);
+        throw `unknown game type: ${gameType}.Aborting!`;
+    }
 }
 
 function checkAnswer(){
@@ -33,8 +50,10 @@ function incrementWrongAnswer(){
 
 }
 
-function displayAdditionAnswer(){
-
+function displayAdditionQuestion(operand1, operand2){
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent= operand2;
+    document.getElementById('operator').textContent = "+";
 }
 
 function displayMultiplyQuestion(){
